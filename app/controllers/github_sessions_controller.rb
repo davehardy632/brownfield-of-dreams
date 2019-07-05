3a7d7323531cd3a066aa5ec98e9fbb1aafbb4a3b
David Hardy <davehardy632@gmail.com>
2019-07-05 16:40:51 -0600

    refactor gihub sessions controller

--- app/controllers/github_sessions_controller.rb
+++ app/controllers/github_sessions_controller.rb
@@ -1,11 +1,9 @@
 class GithubSessionsController < ApplicationController
 
   def create
-    if user = User.find(current_user.id)
-      token = user.from_omniauth(request.env["omniauth.auth"])
-      current_user.update_column(:token, token)
-    end
+    token = current_user.from_omniauth(request.env["omniauth.auth"])
+    current_user.update_column(:token, token)
+
     redirect_to dashboard_path
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -13,14 +13,3 @@ class User < ApplicationRecord
     auth_info["credentials"]["token"]
   end
 end
-# create_table "users", force: :cascade do |t|
-#   t.string "email"
-#   t.string "first_name"
-#   t.string "last_name"
-#   t.string "password_digest"
-#   t.integer "role", default: 0
-#   t.datetime "created_at", null: false
-#   t.datetime "updated_at", null: false
-#   t.string "token"
-#   t.index ["email"], name: "index_users_on_email"
-# end
