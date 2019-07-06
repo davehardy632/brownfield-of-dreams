9d0c5a9e7cbe205662f57e8452804939a160921e
David Hardy <davehardy632@gmail.com>
2019-07-06 12:33:27 -0600

    card 10 completed, a user who is not logged cannot see tutorials whos classroom attribute is true and is referred to as classroom content

--- spec/features/user/user_must_login_for_tutorial_content_spec.rb
+++ spec/features/user/user_must_login_for_tutorial_content_spec.rb
@@ -6,29 +6,16 @@ describe "As a user who is not logged in" do
     @tutorial_1 = create(:tutorial, classroom: true)
     @tutorial_2 = create(:tutorial, classroom: true)
     @tutorial_3 = create(:tutorial, classroom: false)
+    @tutorial_4 = create(:tutorial, classroom: false)
   end
-  it "I cannot see tutorials marked with 'classroom content'" do
+  it "I cannot see tutorials marked with 'classroom content' from the root path" do
 
     visit root_path
 
     expect(page).to have_content(@tutorial_3.title)
+    expect(page).to have_content(@tutorial_4.title)
     expect(page).to_not have_content(@tutorial_1.title)
     expect(page).to_not have_content(@tutorial_2.title)
 
-    visit tutorials_path
-
-    expect(page).to have_content(@tutorial_3.title)
-    expect(page).to_not have_content(@tutorial_1.title)
-    expect(page).to_not have_content(@tutorial_2.title)
-
-    visit tutorial_path(@tutorial_1)
-
-    expect(page).to_not have_content(@tutorial_1.title)
   end
 end
-
-
-
-# Currently all tutorials are visible to anyone. We want to make tutorials marked as "classroom content" viewable only if the user is logged in.
-#
-# The tutorials table has a boolean column for classroom that should be used for this story.
--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -7,8 +7,9 @@ RSpec.describe Tutorial, type: :model do
       tutorial_1 = create(:tutorial, classroom: true)
       tutorial_2 = create(:tutorial, classroom: true)
       tutorial_3 = create(:tutorial, classroom: false)
+      tutorial_4 = create(:tutorial, classroom: false)
 
-      expect(Tutorial.non_classroom).to eq([tutorial_3])
+      expect(Tutorial.non_classroom).to eq([tutorial_3, tutorial_4])
     end
   end
 end
