0c417d6daf3da87ca24d45b5f83506b6504144a1
Noah Flint <noah.flint24@gmail.com>
2019-07-09 16:09:23 -0600

    created update_positions class method

--- app/models/video.rb
+++ app/models/video.rb
@@ -7,4 +7,12 @@ class Video < ApplicationRecord
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
+
+  def self.update_positions
+    all.map do |video|
+      if video.position == nil
+        video.update_column(:position, 0)
+      end
+    end
+  end
 end
