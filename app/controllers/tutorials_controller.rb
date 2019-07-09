5c903ec0745c08e9fb7955406c89948e34464013
Noah Flint <noah.flint24@gmail.com>
2019-07-09 11:38:23 -0600

    added conditional to show action to only check if has_videos?

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,8 +3,6 @@ class TutorialsController < ApplicationController
     tutorial = Tutorial.find(params[:id])
     if tutorial.has_videos?
       @facade = TutorialFacade.new(tutorial, params[:video_id])
-    elsif current_admin?
-      redirect_to admin_tutorial_videos_path
     else
       flash[:alert] = 'This tutorial has no videos at this time.'
       redirect_to root_path
