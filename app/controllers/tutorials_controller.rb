8cab53de37835a67c42c274840afbdfd6a8b4cad
Noah Flint <noah.flint24@gmail.com>
2019-07-08 21:31:22 -0600

    added redirects depending on current_user

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -3,8 +3,11 @@ class TutorialsController < ApplicationController
     tutorial = Tutorial.find(params[:id])
     if tutorial.has_videos?
       @facade = TutorialFacade.new(tutorial, params[:video_id])
+    elsif current_admin?
+      redirect_to admin_tutorial_videos_path
     else
-      redirect_to new_video_path
+      flash[:alert] = 'This tutorial has no videos at this time.'
+      redirect_to root_path
     end
   end
 end
