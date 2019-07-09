6033d44269a6fae9e0c065c291d1ef66589c2d7c
Noah Flint <noah.flint24@gmail.com>
2019-07-09 11:34:57 -0600

    now tests for anykind of user

--- spec/features/vistors/visitor_tutorial_show_page_spec.rb
+++ spec/features/vistors/visitor_tutorial_show_page_spec.rb
@@ -1,11 +1,9 @@
 require 'rails_helper'
 
-describe 'as a visitor' do
+describe 'as a any user' do
   describe 'on the home page' do
     context 'when it click on a tutorials show page with no videos' do
       it 'is shown a message that there are no videos currently' do
-        user = create(:user)
-        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
         tutorial1 = create(:tutorial)
         tutorial2 = create(:tutorial)
         video1 = create(:video, tutorial_id: tutorial2.id)
