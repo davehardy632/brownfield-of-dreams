78e1da337b6077191c022e178daf67ac2ad1bc61
Noah Flint <noah.flint24@gmail.com>
2019-07-09 13:41:13 -0600

    added validation to video model

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,9 @@
 class Video < ApplicationRecord
+
+  validates_presence_of :title,
+                        :description,
+                        :position
+
   has_many :user_videos
   has_many :users, through: :user_videos
   belongs_to :tutorial
