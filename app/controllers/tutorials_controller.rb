dbc591ec3461615e6a2f612fc10e668fcbbc691b
Noah Flint <noah.flint24@gmail.com>
2019-07-08 19:47:50 -0600

    added conditional to show action

--- app/controllers/tutorials_controller.rb
+++ app/controllers/tutorials_controller.rb
@@ -1,6 +1,10 @@
 class TutorialsController < ApplicationController
   def show
     tutorial = Tutorial.find(params[:id])
-    @facade = TutorialFacade.new(tutorial, params[:video_id])
+    if tutorial.has_videos?
+      @facade = TutorialFacade.new(tutorial, params[:video_id])
+    else
+      redirect_to new_video_path
+    end
   end
 end
