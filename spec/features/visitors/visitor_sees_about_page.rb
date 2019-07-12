51f7ed7633ab39b02080819baa5da3d5c2f60159
Noah Flint <noah.flint24@gmail.com>
2019-07-12 16:04:16 -0600

    test for about page and get started page

--- spec/features/visitors/visitor_sees_about_page.rb
+++ spec/features/visitors/visitor_sees_about_page.rb
@@ -7,8 +7,17 @@ RSpec.describe 'as a visitor' do
 
       click_link 'About'
       
-      exepect(current_path).to eq(about_path)
-      exepect(page).to have_content('This application is designed')
+      # this hits a pry in the about#show, but is not coverd by simplecov?
+      expect(current_path).to eq(about_path)
+      expect(page).to have_content('This application is designed')
+    end
+    it 'sees a link to the get started page on the about page' do
+      visit about_path
+
+      click_link 'Get Started'
+
+      expect(current_path).to eq(get_started_path)
+      expect(page).to have_content('Get Started')
     end
   end
 end
