2fbf9587224452775492c093c8ae4b0e29b88f92
Noah Flint <noah.flint24@gmail.com>
2019-07-12 20:38:31 -0600

    refactored index method

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,13 +1,14 @@
 class WelcomeController < ApplicationController
   def index
     if params[:tag] && !current_user.nil?
-      @tutorials = Tutorial.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+      @tutorials = Tutorial.tagged_with(params[:tag])
+                           .paginate(page: params[:page], per_page: 5)
     elsif !current_user.nil?
-      @tutorials = Tutorial.all.paginate(page: params[:page], per_page: 5)
-    elsif params[:tag] && current_user.nil?
-      @tutorials = Tutorial.non_classroom.tagged_with(params[:tag]).paginate(page: params[:page], per_page: 5)
+      @tutorials = Tutorial.all
+                           .paginate(page: params[:page], per_page: 5)
     elsif current_user.nil?
-      @tutorials = Tutorial.non_classroom.paginate(page: params[:page], per_page: 5)
+      @tutorials = Tutorial.non_classroom
+                           .paginate(page: params[:page], per_page: 5)
     end
   end
 end
