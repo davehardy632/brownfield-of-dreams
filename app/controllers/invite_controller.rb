06ba0b326115e93b9a6e19e06b0b5bd3c1f2ea5a
David Hardy <davehardy632@gmail.com>
2019-07-12 22:16:23 -0600

    email invites complete except for some testing cases..... wip

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -8,8 +8,14 @@ class InviteController < ApplicationController
     user_handle = params["email"]
     github_service = GithubApiService.new(current_user.token)
     response = github_service.user_email(user_handle)
-    user_email = response[:email]
-    InviteMailer.invite(user_email, user_handle, current_user).deliver_now
-    redirect_to dashboard_path
+      if response[:email] != nil
+        user_email = response[:email]
+        InviteMailer.invite(user_email, user_handle, current_user).deliver_now
+        flash[:notice] = "Successfully sent invite!"
+        redirect_to dashboard_path
+      else
+        flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
+        redirect_to dashboard_path
+      end
+    end
   end
-end
