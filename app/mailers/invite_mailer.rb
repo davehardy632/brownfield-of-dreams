1ef6c8a49968af1e2389a439c1797e428680626e
Noah Flint <noah.flint24@gmail.com>
2019-07-13 15:16:55 -0600

    removed double quotes

--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -2,6 +2,6 @@ class InviteMailer < ApplicationMailer
   def invite(email, handle, invitee)
     @handle = handle
     @invitee = invitee
-    mail(to: email, subject: "User Invitation")
+    mail(to: email, subject: 'User Invitation')
   end
 end
