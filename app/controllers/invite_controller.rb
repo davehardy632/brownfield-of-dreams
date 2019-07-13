20d7b65006e15360b1e70beb8242d4739034c594
Noah Flint <noah.flint24@gmail.com>
2019-07-13 15:16:08 -0600

    moved redirect to bottom of method

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -1,19 +1,17 @@
 class InviteController < ApplicationController
-
   def new; end
 
   def create
-    user_handle = params["handle"]
+    user_handle = params['handle']
     github_service = GithubApiService.new(current_user.token)
     response = github_service.user_email(user_handle)
-      if response[:email] != nil
-        user_email = response[:email]
-        InviteMailer.invite(user_email, user_handle, current_user).deliver_now
-        flash[:notice] = "Successfully sent invite!"
-        redirect_to dashboard_path
-      else
-        flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
-        redirect_to dashboard_path
-      end
+    if !response[:email].nil?
+      user_email = response[:email]
+      InviteMailer.invite(user_email, user_handle, current_user).deliver_now
+      flash[:notice] = 'Successfully sent invite!'
+    else
+      flash[:notice] = "The Github user you selected doesn't have an email address associated with their account."
     end
+    redirect_to dashboard_path
   end
+end
