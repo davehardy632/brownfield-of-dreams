9619e991b78c7b09c8c7620cfd9ff1996fd405a6
Noah Flint <noah.flint24@gmail.com>
2019-07-13 15:55:30 -0600

    refactored method length

--- app/controllers/invite_controller.rb
+++ app/controllers/invite_controller.rb
@@ -3,8 +3,10 @@ class InviteController < ApplicationController
 
   def create
     user_handle = params['handle']
-    github_service = GithubApiService.new(current_user.token)
-    response = github_service.user_email(user_handle)
+    # github_service = GithubApiService.new(current_user.token)
+    # response = github_service.user_email(user_handle)
+
+    response = GithubApiService.new(current_user.token).user_email(user_handle)
     if !response[:email].nil?
       user_email = response[:email]
       InviteMailer.invite(user_email, user_handle, current_user).deliver_now
