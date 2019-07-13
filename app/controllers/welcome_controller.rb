93ecc3f85d49ea8bfcd56f0e85a378c5f74bc17c
David Hardy <davehardy632@gmail.com>
2019-07-13 15:46:00 -0600

    refactor if logic in welcome controller

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
     if params[:tag] && !current_user.nil?
       @tutorials = Tutorial.tagged_with(params[:tag])
                            .paginate(page: params[:page], per_page: 5)
-    elsif !current_user.nil?
+    elsif current_user
       @tutorials = Tutorial.all
                            .paginate(page: params[:page], per_page: 5)
     elsif current_user.nil?
